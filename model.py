8cb7e8d83d01273078724e180a513df72bf648fc
Mahesh <2017.mahesh.keswani@ves.ac.in>
2020-03-06 23:12:28 +0530

    added the silhouette scores for samples

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-# Hierarchical Clustering
 
 # Importing the libraries
 import pandas as pd 
@@ -13,53 +12,95 @@ X = dataset.iloc[:, :].values
 # Constants
 file_name_to_save = 'facbook_edge_data_model.sav'
 
-# Using the dendrogram to find the optimal number of clusters
-import scipy.cluster.hierarchy as sch
-dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
-plt.title('Dendrogram')
-plt.xlabel('Edge ( FROM )')
-plt.ylabel('Edge ( TO )')
-plt.show()
+# # Using the dendrogram to find the optimal number of clusters
+# import scipy.cluster.hierarchy as sch
+# dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+# plt.title('Dendrogram')
+# plt.xlabel('Edge ( FROM )')
+# plt.ylabel('Edge ( TO )')
+# plt.show()
 
 # Fitting Hierarchical Clustering to the dataset
-from sklearn.cluster import AgglomerativeClustering
-hc = AgglomerativeClustering(n_clusters = 10, affinity = 'euclidean', linkage = 'ward')
-y_hc = hc.fit_predict(X)
+# from sklearn.cluster import AgglomerativeClustering
+# hc = AgglomerativeClustering(n_clusters = 10, affinity = 'euclidean', linkage = 'ward')
+# y_hc = hc.fit_predict(X)
 
 # Using the elbow method to find the optimal number of clusters
-from sklearn.cluster import KMeans
-wcss = []
-for i in range(1, 25):
-    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
-    kmeans.fit(X)
-    wcss.append(kmeans.inertia_)
-plt.plot(range(1, 11), wcss)
-plt.title('The Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.show()
+# from sklearn.cluster import KMeans
+# wcss = []
+# for i in range(1, 25):
+#     kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+#     kmeans.fit(X)
+#     wcss.append(kmeans.inertia_)
+# plt.plot(range(1, 11), wcss)
+# plt.title('The Elbow Method')
+# plt.xlabel('Number of clusters')
+# plt.ylabel('WCSS')
+# plt.show()
 
 # Fitting K-Means to the dataset
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score, silhouette_samples
+
 kmeans = KMeans(n_clusters = 10, init = 'k-means++', random_state = 42)
 y_kmeans = kmeans.fit_predict(X)
-pickle.dump(kmeans, open(file_name_to_save, 'wb'))
+
+silhouette_avg = silhouette_score(X, y_kmeans)
+print('For n(10) clusters The average silhouette score is {}'.format(silhouette_avg))
+
+# sample_silhouette_values = silhouette_samples(X, y_kmeans)
+# with open('silhouette_scores_for_samples.txt', 'w') as f:
+#     for value in sample_silhouette_values:
+#         f.write(str(value)+"\n")
+#     print('File saved')
+
+# print(sample_silhouette_values)
+# pickle.dump(kmeans, open(file_name_to_save, 'wb'))
 
 print("Model Saved Done")
 
 # Visualising the clusters
-plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 50, c = 'red', label = 'Cluster 1')
-plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 50, c = 'blue', label = 'Cluster 2')
-plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 50, c = 'green', label = 'Cluster 3')
-plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 50, c = 'cyan', label = 'Cluster 4')
-plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 50, c = 'magenta', label = 'Cluster 5')
-plt.scatter(X[y_kmeans == 5, 0], X[y_kmeans == 5, 1], s = 50, c = 'orange', label = 'Cluster 6')
-plt.scatter(X[y_kmeans == 6, 0], X[y_kmeans == 6, 1], s = 50, c = 'purple', label = 'Cluster 7')
-plt.scatter(X[y_kmeans == 7, 0], X[y_kmeans == 7, 1], s = 50, c = 'lime', label = 'Cluster 8')
-plt.scatter(X[y_kmeans == 8, 0], X[y_kmeans == 8, 1], s = 50, c = 'indigo', label = 'Cluster 9')
-plt.scatter(X[y_kmeans == 9, 0], X[y_kmeans == 9, 1], s = 50, c = 'pink', label = 'Cluster 10')
+# plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 50, c = 'red', label = 'Cluster 1')
+# plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 50, c = 'blue', label = 'Cluster 2')
+# plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 50, c = 'green', label = 'Cluster 3')
+# plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 50, c = 'cyan', label = 'Cluster 4')
+# plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 50, c = 'magenta', label = 'Cluster 5')
+# plt.scatter(X[y_kmeans == 5, 0], X[y_kmeans == 5, 1], s = 50, c = 'orange', label = 'Cluster 6')
+# plt.scatter(X[y_kmeans == 6, 0], X[y_kmeans == 6, 1], s = 50, c = 'purple', label = 'Cluster 7')
+# plt.scatter(X[y_kmeans == 7, 0], X[y_kmeans == 7, 1], s = 50, c = 'lime', label = 'Cluster 8')
+# plt.scatter(X[y_kmeans == 8, 0], X[y_kmeans == 8, 1], s = 50, c = 'indigo', label = 'Cluster 9')
+# plt.scatter(X[y_kmeans == 9, 0], X[y_kmeans == 9, 1], s = 50, c = 'pink', label = 'Cluster 10')
 
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 100, c = 'yellow', label = 'Centroids')
-plt.title('Clusters of social groups')
-plt.legend()
-plt.show()
+# plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 100, c = 'yellow', label = 'Centroids')
+# plt.title('Clusters of social groups')
+# plt.legend()
+# plt.show()
+
+
+
+'''
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.evaluation import ClusteringEvaluator
+
+# Loads data.
+dataset = spark.read.format("libsvm").load("facebook_combined.txt")
+
+# Trains a k-means model.
+kmeans = KMeans().setK(10).setSeed(1)
+model = kmeans.fit(dataset)
+
+# Make predictions
+predictions = model.transform(dataset)
+
+# Evaluate clustering by computing Silhouette score
+evaluator = ClusteringEvaluator()
+
+silhouette = evaluator.evaluate(predictions)
+print("Silhouette with squared euclidean distance = " + str(silhouette))
+
+# Shows the result.
+centers = model.clusterCenters()
+print("Cluster Centers: ")
+for center in centers:
+    print(center)
+'''
