7594626d1ac572db285cbd6af3a05125dcec90f1
Mahesh <2017.mahesh.keswani@ves.ac.in>
2020-03-06 19:20:22 +0530

    passed the data from backenend to the ui

--- clouderaproject/clouderaproject/views.py
+++ clouderaproject/clouderaproject/views.py
@@ -4,10 +4,18 @@ from nodes.models import Node
 from edges.models import Edge
 
 def home(request):
-    clusters = []
+    cluster1 = Edge.objects.filter(cluster = 1)
+    cluster2 = Edge.objects.filter(cluster = 2)
+    cluster3 = Edge.objects.filter(cluster = 3)
+    cluster4 = Edge.objects.filter(cluster = 4)
+    cluster5 = Edge.objects.filter(cluster = 5)
+    cluster6 = Edge.objects.filter(cluster = 6)
+    cluster7 = Edge.objects.filter(cluster = 7)
+    cluster8 = Edge.objects.filter(cluster = 8)
+    cluster9 = Edge.objects.filter(cluster = 9)
+    cluster10 = Edge.objects.filter(cluster = 10)
+    
 
-    for i in range(0, 10):
-        clusters.append(Edge.objects.filter(cluster=i))
-
-    # return HttpResponse(clusters[0][0].from_edge)
-    return render(request, 'home.html', {'clusters' : clusters})
+    return render(request, 'home.html', {'cluster1' : cluster1, 'cluster2':cluster2, 'cluster3':cluster3,
+                                        'cluster4':cluster4, 'cluster5':cluster5, 'cluster6':cluster6,
+                                        'cluster7':cluster7, 'cluster8':cluster8, 'cluster9': cluster9, 'cluster10':cluster10})
