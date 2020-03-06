9c1ef4dd1ea98309d2a0f1c8827f7e3dfb38d16b
Mahesh <2017.mahesh.keswani@ves.ac.in>
2020-03-06 23:10:07 +0530

    added url for new feature analysis

--- clouderaproject/clouderaproject/urls.py
+++ clouderaproject/clouderaproject/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = "home"),
+    path('analysis/', views.analysis, name = "analysis"),
+    
 ]
